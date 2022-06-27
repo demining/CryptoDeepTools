e01a7d9330825e0ef650473c65aa3b62cfd57b3e
CRYPTO DEEP TECH <87700947+demining@users.noreply.github.com>
2022-06-27 19:39:56 +0300

    Add files via upload

---
+++ 02BreakECDSAcryptography/getsign.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cat RawTX.json > index.json
+for run in {1..4}; do
+    export LINE=1 ; sed -n "${LINE}p" index.json > index2.json
+    sed -i '1d' index.json
+    echo "with open('index2.json') as myfile:" >> fileopen.py
+    echo "    listfile='\n'.join(f'python2 breakECDSA.py {line.rstrip()[:]} >> signatures.json' for line in myfile)" >> fileopen.py
+    echo "f = open('signscript.sh', 'a')" >> fileopen.py
+    echo "f.write('#!/bin/bash' + '\n')" >> fileopen.py
+    echo "f.write('' + listfile + '' + '\n')" >> fileopen.py
+    echo "f.close()" >> fileopen.py
+    python3 fileopen.py
+    rm index2.json
+    chmod +x signscript.sh
+    ./signscript.sh
+    rm signscript.sh
+    rm fileopen.py
+    rm index.json
+done
