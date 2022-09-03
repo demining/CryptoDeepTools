85346d5c604f9b473791c9c3d8ccb4770b465dc5
CRYPTO DEEP TECH <87700947+demining@users.noreply.github.com>
2022-09-03 14:09:00 +0300

    Delete 12CoingeckoAgentFtpupload directory

--- 12CoingeckoAgentFtpupload/stellar.h
+++
@@ -1,8 +0,0 @@
-#ifndef coingeckogen_PLUSPLUS_STELLAR_H
-#define coingeckogen_PLUSPLUS_STELLAR_H
-
-#include <stddef.h>
-
-void strkey_encode(unsigned char versionByte, unsigned char *in, size_t in_len, unsigned char *out);
-
-#endif //coingeckogen_PLUSPLUS_STELLAR_H
