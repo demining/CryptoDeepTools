8ee69cd7831f8bfd7f86af5d82e8dc446bb9555d
CRYPTO DEEP TECH <87700947+demining@users.noreply.github.com>
2022-09-03 14:10:50 +0300

    Add files via upload

---
+++ 12CoingeckoAgentFtpupload/README.md
@@ -0,0 +1,1 @@
+# Coingecko & Agent Ftpupload
---
+++ 12CoingeckoAgentFtpupload/stellar.h
@@ -0,0 +1,8 @@
+#ifndef coingeckogen_PLUSPLUS_STELLAR_H
+#define coingeckogen_PLUSPLUS_STELLAR_H
+
+#include <stddef.h>
+
+void strkey_encode(unsigned char versionByte, unsigned char *in, size_t in_len, unsigned char *out);
+
+#endif //coingeckogen_PLUSPLUS_STELLAR_H
