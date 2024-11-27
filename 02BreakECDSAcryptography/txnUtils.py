7dfdb4e14aaf9e9fea136dc3c616d69d78476de7
CRYPTO DEEP TECH <87700947+demining@users.noreply.github.com>
2024-11-28 01:27:21 +0300

    Update txnUtils.py

--- 02BreakECDSAcryptography/txnUtils.py
+++ 02BreakECDSAcryptography/txnUtils.py
@@ -1,5 +1,5 @@
 import ecdsa
-import hashlib
+from Crypto.Hash import SHA256
 import struct
 import unittest
 import utils
@@ -8,101 +8,85 @@ import base58
 
 # Makes a transaction from the inputs
 # outputs is a list of [redemptionSatoshis, outputScript]
-
 def makeRawTransaction(outputTransactionHash, sourceIndex, scriptSig, outputs):
     def makeOutput(data):
         redemptionSatoshis, outputScript = data
-        return (struct.pack("<Q", redemptionSatoshis).encode('hex') +
-        '%02x' % len(outputScript.decode('hex')) + outputScript)
+        return (struct.pack("<Q", redemptionSatoshis).hex() +
+                '%02x' % len(outputScript.decode('hex')) + outputScript)
+    
     formattedOutputs = ''.join(map(makeOutput, outputs))
 
-    return ("01000000" + # 4 bytes version
-        "01" + # varint for number of inputs
-        outputTransactionHash.decode('hex')[::-1].encode('hex') + # reverse outputTransactionHash
-        struct.pack('<L', sourceIndex).encode('hex') +
-        '%02x' % len(scriptSig.decode('hex')) + scriptSig +
-        "ffffffff" + # sequence
-        "%02x" % len(outputs) + # number of outputs
-        formattedOutputs +
-        "00000000" # lockTime
-        )
-
+    return ("01000000" +  # 4 bytes version
+            "01" +  # varint for number of inputs
+            outputTransactionHash.decode('hex')[::-1].hex() +  # reverse outputTransactionHash
+            struct.pack('<L', sourceIndex).hex() +
+            '%02x' % len(scriptSig.decode('hex')) + scriptSig +
+            "ffffffff" +  # sequence
+            "%02x" % len(outputs) +  # number of outputs
+            formattedOutputs +
+            "00000000"  # lockTime
+           )
 
 # Returns [first, sig, pub, rest]
-
 def parseTxn(txn):
-    first = txn[0:41*2]
-    scriptLen = int(txn[41*2:42*2], 16)
-    script = txn[42*2:42*2+2*scriptLen]
+    first = txn[0:41 * 2]
+    scriptLen = int(txn[41 * 2:42 * 2], 16)
+    script = txn[42 * 2:42 * 2 + 2 * scriptLen]
     sigLen = int(script[0:2], 16)
-    sig = script[2:2+sigLen*2]
-    pubLen = int(script[2+sigLen*2:2+sigLen*2+2], 16)
-    pub = script[2+sigLen*2+2:]
-    assert(len(pub) == pubLen*2)
-    rest = txn[42*2+2*scriptLen:]
-    return [first, sig, pub, rest]         
-
-
+    sig = script[2:2 + sigLen * 2]
+    pubLen = int(script[2 + sigLen * 2:2 + sigLen * 2 + 2], 16)
+    pub = script[2 + sigLen * 2 + 2:]
+    assert(len(pub) == pubLen * 2)
+    rest = txn[42 * 2 + 2 * scriptLen:]
+    return [first, sig, pub, rest]
 
 # Substitutes the scriptPubKey into the transaction, appends SIGN_ALL to make the version
-
 # of the transaction that can be signed
-
 def getSignableTxn(parsed):
     first, sig, pub, rest = parsed
     inputAddr = base58.b58decode_check(keyUtils.pubKeyToAddr(pub))
-    return first + "1976a914" + inputAddr.encode('hex') + "88ac" + rest + "01000000"
-
-
+    return first + "1976a914" + inputAddr.hex() + "88ac" + rest + "01000000"
 
 # Verifies that a transaction is properly signed, assuming the generated scriptPubKey matches
-
 # the one in the previous transaction's output
-
-def verifyTxnSignature(txn):                    
-
-    parsed = parseTxn(txn)      
-
+def verifyTxnSignature(txn):
+    parsed = parseTxn(txn)
     signableTxn = getSignableTxn(parsed)
 
-    hashToSign = hashlib.sha256(hashlib.sha256(signableTxn.decode('hex')).digest()).digest().encode('hex')
+    # Calculate double SHA-256 hash using pycryptodome
+    hash1 = SHA256.new(signableTxn.decode('hex')).digest()
+    hashToSign = SHA256.new(hash1).digest().hex()
 
-    assert(parsed[1][-2:] == '01') # hashtype
+    assert(parsed[1][-2:] == '01')  # hashtype
 
     sig = keyUtils.derSigToHexSig(parsed[1][:-2])
-
     public_key = parsed[2]
 
-    vk = ecdsa.VerifyingKey.from_string(public_key[2:].decode('hex'), curve=ecdsa.SECP256k1)
+    vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(public_key[2:]), curve=ecdsa.SECP256k1)
 
-    assert(vk.verify_digest(sig.decode('hex'), hashToSign.decode('hex')))
-
-
+    assert(vk.verify_digest(bytes.fromhex(sig), bytes.fromhex(hashToSign)))
 
 def makeSignedTransaction(privateKey, outputTransactionHash, sourceIndex, scriptPubKey, outputs):
+    myTxn_forSig = (makeRawTransaction(outputTransactionHash, sourceIndex, scriptPubKey, outputs) +
+                    "01000000")  # hash code
 
-    myTxn_forSig = (makeRawTransaction(outputTransactionHash, sourceIndex, scriptPubKey, outputs)
+    # Calculate double SHA-256 hash using pycryptodome
+    hash1 = SHA256.new(myTxn_forSig.decode('hex')).digest()
+    s256 = SHA256.new(hash1).digest()
 
-         + "01000000") # hash code
-
-
-
-    s256 = hashlib.sha256(hashlib.sha256(myTxn_forSig.decode('hex')).digest()).digest()
-
-    sk = ecdsa.SigningKey.from_string(privateKey.decode('hex'), curve=ecdsa.SECP256k1)
-
-    sig = sk.sign_digest(s256, sigencode=ecdsa.util.sigencode_der) + '\01' # 01 is hashtype
+    sk = ecdsa.SigningKey.from_string(bytes.fromhex(privateKey), curve=ecdsa.SECP256k1)
+    
+    sig = sk.sign_digest(s256, sigencode=ecdsa.util.sigencode_der) + b'\01'  # b'\01' is hashtype
 
     pubKey = keyUtils.privateKeyToPublicKey(privateKey)
 
-    scriptSig = utils.varstr(sig).encode('hex') + utils.varstr(pubKey.decode('hex')).encode('hex')
+    scriptSig = utils.varstr(sig).hex() + utils.varstr(pubKey.decode('hex')).hex()
 
     signed_txn = makeRawTransaction(outputTransactionHash, sourceIndex, scriptSig, outputs)
 
     verifyTxnSignature(signed_txn)
 
     return signed_txn
-
     
 
 class TestTxnUtils(unittest.TestCase):
